f6895e547cb8a521339296e15f94b8f0af76a881
Corey Werner <cnotethegr8@gmail.com>
2018-12-23 10:47:16 +0200

    included helper for testing

--- KinMigrationModule/KinMigrationModuleTests/KinMigrationModuleTests.swift
+++ KinMigrationModule/KinMigrationModuleTests/KinMigrationModuleTests.swift
@@ -9,7 +9,50 @@
 import XCTest
 @testable import KinMigrationModule
 
+class KinMigrationModuleHelper: NSObject {
+    let migrationManager = KinMigrationManager()
+    let network: Network
+    let appIdValue: String
+    let promise: Promise<KinClientProtocol>
+
+    init(blockchainURL: URL, network: Network = .testNet, appIdValue: String = "test", promise: Promise<KinClientProtocol>) {
+        self.network = network
+        self.appIdValue = appIdValue
+        self.promise = promise
+
+        super.init()
+
+        migrationManager.delegate = self
+
+        do {
+            try migrationManager.start(withVersionURL: blockchainURL)
+        }
+        catch {
+            promise.signal(error)
+        }
+    }
+}
+
+extension KinMigrationModuleHelper: KinMigrationManagerDelegate {
+    func kinMigrationManagerCanCreateClient(_ kinMigrationManager: KinMigrationManager, factory: KinClientFactory) {
+        do {
+            let appId = try AppId("test")
+            let client = factory.KinClient(network: network, appId: appId)
+            promise.signal(client)
+        }
+        catch {
+            promise.signal(error)
+        }
+    }
+
+    func kinMigrationManagerError(_ kinMigrationManager: KinMigrationManager, error: Error) {
+        promise.signal(error)
+    }
+}
+
 class KinMigrationModuleTests: XCTestCase {
+    let kinCoreBlockchainURL = URL(string: "http://www.mocky.io/v2/5c18b4642f00005300af10e2")!
+    let kinSDKBlockchainURL = URL(string: "http://www.mocky.io/v2/5c18b46b2f00006500af10e4")!
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -19,9 +62,27 @@ class KinMigrationModuleTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
+    func kinCoreClient() -> Promise<KinClientProtocol> {
+        let migrationHelper = KinMigrationModuleHelper(blockchainURL: kinCoreBlockchainURL, promise: Promise<KinClientProtocol>())
+        return migrationHelper.promise
+    }
+
+    func kinSDKClient() -> Promise<KinClientProtocol> {
+        let migrationHelper = KinMigrationModuleHelper(blockchainURL: kinSDKBlockchainURL, promise: Promise<KinClientProtocol>())
+        return migrationHelper.promise
+    }
+
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let expectation = self.expectation(description: "Create Kin Client")
+
+        kinSDKClient()
+            .then { kinClient in
+                XCTAssertNotNil(kinClient)
+                expectation.fulfill()
+            }
+            .error { error in
+                XCTFail()
+        }
     }
 
     func testPerformanceExample() {
